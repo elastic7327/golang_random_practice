eebfba012f44a97a0f9b8df66cee8ecaa393d5ae
elastic7327 <elastic7327@gmail.com>
2018-05-06 13:06:33 +0900

    u32 update

--- interfaces/interfaces_u32_duck_typing_test.go
+++ interfaces/interfaces_u32_duck_typing_test.go
@@ -1,7 +1,57 @@
 package interfaces
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+type Duck struct {
+}
+
+func (d Duck) quack() {
+	fmt.Println("Quaacckkk!")
+}
+
+func (d Duck) feathers() {
+	fmt.Println("Duck has white and gray ...")
+}
+
+type Person struct {
+}
+
+func (p Person) quack() {
+	fmt.Println("(Mocking) -- Quaacckkk! #$@@#$?")
+}
+
+func (p Person) feathers() {
+	fmt.Println("Person ..gray .. white..")
+}
+
+type Quacker interface {
+	/* TODO: add methods */
+	quack()
+	feathers()
+}
+
+func inTheForest(q Quacker) {
+	q.quack()
+	q.feathers()
+}
 
 func TestDuckTypingExample(t *testing.T) {
 
+	var donald Duck
+	var john Person
+
+	inTheForest(donald)
+	inTheForest(john)
+}
+
+func TestDuckTestInterfaceBool(t *testing.T) {
+
+	var donald Duck
+
+	if v, ok := interface{}(donald).(Quacker); ok {
+		fmt.Println(v, ok)
+	}
 }
